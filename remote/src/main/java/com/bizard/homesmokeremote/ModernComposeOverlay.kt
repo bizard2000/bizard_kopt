fd6d55e702dc53a2e68efb485c4b8fe262fb95d6
bizard2000 <bizard2000@gmail.com>
2026-09-08 14:26:41 +0900

    Use public ComposeView content API

--- remote/src/main/java/com/bizard/homesmokeremote/ModernComposeOverlay.kt
+++ remote/src/main/java/com/bizard/homesmokeremote/ModernComposeOverlay.kt
@@ -52,7 +52,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
